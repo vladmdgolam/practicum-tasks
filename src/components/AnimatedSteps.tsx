89746dccf7613c2d217c160d384b4f5499bc493a
Vlad <vladmdgolam@ya.ru>
2021-10-04 11:37:12 +0300

    docs: upd comments

--- src/components/AnimatedSteps.tsx
+++ src/components/AnimatedSteps.tsx
@@ -8,6 +8,8 @@ const AnimatedSteps = ({ index = 0 }) => {
   return (
     <motion.svg width={700} height={248} fill="none">
       {steps.map((Step, i) => (
+        // тут можно принимать коллбек от "шага", что анимации закончились
+        // и не рендерить невидимые шаги
         <Step key={i} visible={i === index} />
       ))}
     </motion.svg>
